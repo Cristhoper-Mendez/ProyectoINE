b698b65a563a671f17189a5d225352a5654e7547
Fernando José Barraza Álvarez <fernando.jba.2000@gmail.com>
2025-06-24 22:46:30 -0600

    Ajustes Controladores

--- ProyectoINE/Models/ViewModels/TipoViewModel.cs
+++ ProyectoINE/Models/ViewModels/TipoViewModel.cs
@@ -18,11 +18,11 @@ namespace ProyectoINE.Models.ViewModels
         public decimal ValorResidual { get; set; }
 
         [Display(Name = "Vida útil en años")]
-        [Range(1, 100, ErrorMessage = "Debe ingresar al menos 1 año")]
+        [Range(0, 100, ErrorMessage = "Debe ingresar al menos 0 año")]
         public int VidaUtilAnios { get; set; }
 
         [Display(Name = "Tasa de uso del activo (%)")]
-        [Range(0, 100, ErrorMessage = "Debe estar entre 1 y 100")]
+        [Range(0, 100, ErrorMessage = "Debe estar entre 0 y 100")]
         public int TasaUso { get; set; }
 
         [Display(Name = "Impacto fiscal (%)")]
